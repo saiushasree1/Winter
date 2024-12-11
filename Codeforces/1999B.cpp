9dd9d3beeef468012373f47cea19673d63086b70
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-12-11 09:55:25 +0530

    11-12-2024 Q3

---
+++ Codeforces/1999B.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int f(int a,int b){
+  if(a>b) return 1;
+  if(a==b) return 0;
+  if(a<b) return -1;
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        int ans=0;
+        if(f(a,c)+f(b,d)>0) ans++;
+        if(f(a,d)+f(b,c)>0) ans++;
+        if(f(b,c)+f(a,d)>0) ans++;
+        if(f(b,d)+f(a,c)>0) ans++;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+https://codeforces.com/problemset/problem/1999/B
