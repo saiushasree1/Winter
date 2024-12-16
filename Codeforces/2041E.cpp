80ec01063f7f129dc92c5a55cbba3ad4d89ca30a
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-12-16 20:16:08 +0530

    16-12-2024 Q2

---
+++ Codeforces/2041E.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<3<<endl;
+    cout<<b<<" "<<b<<" "<<3*a-2*b<<endl;
+    return 0;
+}
+https://codeforces.com/problemset/problem/2041/E
