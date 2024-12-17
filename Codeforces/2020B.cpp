a2a1a705133bacfe5737407c724890c4ddd20dd2
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-12-17 14:25:59 +0530

    17-12-2024 Q3

---
+++ Codeforces/2020B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+     long long k;
+     cin>>k;
+    
+     cout<<k+int(sqrtl(k)+0.5)<<endl;
+     return;
+}
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+      solve();
+    }
+    return 0;
+}
+https://codeforces.com/problemset/problem/2020/B
